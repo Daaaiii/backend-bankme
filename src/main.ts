ed5d8b456df5c48c1a0b2c7add3dde9687c540c0
Daiane Bolzan <dainha85@gmail.com>
2023-11-19 16:30:49 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  function setupSwagger(app: INestApplication) {
+    const configService = app.get<ConfigService>(ConfigService);
+    const appName = configService.get('npm_package_name');
+    const appVersion = configService.get('npm_package_version');
+
+    const config = new DocumentBuilder()
+      .setTitle(appName)
+      .setDescription('API de recebíveis do BankMe')
+      .setVersion(appVersion)
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('', app, document);
+  }
+  setupSwagger(app);
+  await app.listen(3000, () => {
+    Logger.log('--Server running on port 3000--');
+  });
 }
 bootstrap();
