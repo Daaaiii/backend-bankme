55e634add2bb61c7859ede82dc3c9ac230978b3f
Daiane Bolzan <dainha85@gmail.com>
2023-11-20 12:37:33 -0300

    feat: add throttler to prevent  brute-force attacks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AssignorsModule } from './assignors/assignors.module';
 import { PrismaService } from './database/prisma.service';
 import { PayableModule } from './payable/payable.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AssignorsModule, PayableModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 3,
+      },
+    ]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => PayableModule),
+    forwardRef(() => AssignorsModule),
+  ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
