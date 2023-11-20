a2a7c8ce68d446ba7451b387ac2a61d34a5437b0
Daiane Bolzan <dainha85@gmail.com>
2023-11-20 12:39:31 -0300

    fix: update database

---
+++ prisma/migrations/20231120130742_add_created_at_and_updated_at/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("id", "login", "name", "password") SELECT "id", "login", "name", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
